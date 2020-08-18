be019012cf9d0d863c9b930464b714029eb9c2bd
Enja Reyes <enja.reyes@gmail.com>
2020-08-18 12:33:11 -0500

    storing recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+	constructor(
+		private dataStorageService: DataStorageService,
+	) {}
+
+	public onSaveData() {
+		this.dataStorageService.storeRecipes();
+	}
+
+	public onFetchData() {
+		this.dataStorageService.fetchRecipes().subscribe();
+	}
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,28 +7,7 @@ import { Subject } from 'rxjs';
 @Injectable()
 export class RecipeService {
 	recipesChanged = new Subject<Recipe[]>();
-
-	private recipes: Recipe[] = [
-		new Recipe(
-			'Apple Pie',
-			'Delicious stuff mmmm yeah',
-			"https://www.cuisinart.com/share/images/recipes/photos/recipe_8553_45006627.jpg",
-			[
-				new Ingredient('Sugar', 3),
-				new Ingredient('Butter', 1),
-			]
-		),
-
-		new Recipe(
-			'Muffins',
-			'Yum muffins mmmm yeah',
-			"https://smittenkitchendotcom.files.wordpress.com/2010/08/perfect-blueberry-muffins.jpg?w=1200",
-			[
-				new Ingredient('Butter', 1),
-				new Ingredient('Blueberries', 2),
-			]
-		),
-	];
+	private recipes: Recipe[] = [];
 
 	constructor(private slService: ShoppingListService) {}
 
@@ -38,6 +17,11 @@ export class RecipeService {
 
 	getRecipe(index: number) {
 		return this.recipes[index];
+	}
+
+	setRecipes(recipes: Recipe[]) {
+		this.recipes = recipes;
+		this.recipesChanged.next(this.recipes.slice());
 	}
 
 	addIngredientsToList(ingredients: Ingredient[]) {
